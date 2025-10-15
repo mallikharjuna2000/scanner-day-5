2e2fdabbf9919460a8f1eb48969a64176c9855bb
mallikharjuna2000 <mallikharjunakollipati@gmail.com>
2025-10-15 11:09:57 +0530

    Add files via upload
    literals demo

---
+++ LiterlasDemo.java
@@ -0,0 +1,13 @@
+public class LiteralsDemo{
+	public static void main(String[] args){
+	int a=20;
+	int b=0b10;
+	int c=010;
+	int d=0x10;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(a+"------"+b+"---"+c+"----"+d);
+	}
+}
